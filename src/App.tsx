b33d179d00edb5bb18036c8ea573c1a6c36fdbca
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-12-28 09:35:25 +0000

    save

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { CounterContext } from './contexts/CounterContext'
+
+function App() {
+  
+
+  return (
+    <>
+    <CounterContext i />
+    </>
+  )
+}
+
+export default App
---
+++ src/contexts/CounterContext.tsx
@@ -0,0 +1,23 @@
+
+import  { createContext, useState } from 'react';
+
+
+const CounterContext = createContext({
+  count: 0,
+  setCount: () => {},
+});
+
+
+const CounterProvider = ({children}) => {
+  const [count, setCount] = useState(0);
+
+
+  return (
+    <CounterContext.Provider value={{ count, setCount  }}>  
+      {children}
+    </CounterContext.Provider>
+  );
+};
+
+export { CounterContext, CounterProvider };
+
